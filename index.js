00a488fd1fd90bc136be0c0c1b5be331cd4ee2ca
Yisiang <ongyisiang98@yahoo.com>
2021-09-08 22:35:06 +0800

    Express Router

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-
+//use morgan development version that will log sufficient information
 app.use(morgan('dev'));
+//implement bodyParse that is a middleware to parse the body of the message and populates the req.body property
+app.use(bodyParser.json());
+
+// mounting the router, any request from /dishes endpoint will be handled by dishRouter
+app.use('/dishes', dishRouter); 
+
+//tells express to serve up the static files from specified directory
 app.use(express.static(__dirname + '/public'));
 
+//default endpoint and render endpoint message if not found any endpoints that give response
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
@@ -17,10 +27,8 @@ app.use((req, res, next) => {
 
 });
 
-
-
+//Creates server
 const server = http.createServer(app);
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
